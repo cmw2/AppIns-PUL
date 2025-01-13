cebca1a6fcd95637cde65f1b1336fbbb1d80c471
Chris Westbrook <westbrook.chris@gmail.com>
2025-01-12 20:40:54 -0500

    initial commit with no app insights

---
+++ src/PartsUnlimitedWebsite/Utils/NullTelemetryProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PartsUnlimited.Utils
+{
+    internal class NullTelemetryProvider : ITelemetryProvider
+    {
+        public void TrackEvent(string message)
+        {
+            // do nothing
+        }
+
+        public void TrackEvent(string message, Dictionary<string, string> properties, Dictionary<string, double> measurements)
+        {
+            // do nothing
+        }
+
+        public void TrackException(Exception exception)
+        {
+            // do nothing
+        }
+
+        public void TrackTrace(string message)
+        {
+            // do nothing
+        }
+    }
+}
